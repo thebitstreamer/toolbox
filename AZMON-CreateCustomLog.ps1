10cc33a0baaf3274d317d465570dc559a3d00ec1
thebitstreamer <thebitstreamer@hotmail.com>
2018-12-17 16:45:19 +0100

    first version

---
+++ AZMON-CreateCustomLog.ps1
@@ -0,0 +1,44 @@
+$ResourceGroup = "<YOUR RESOURCE GROUP NAME HERE>"
+$WorkspaceName = "<YOUR WORKSPACE NAME HERE>" 
+
+# Custom Log to collect
+# Customize "fileSystemLocations" to the path where files are
+# more info here https://docs.microsoft.com/en-us/azure/azure-monitor/platform/powershell-workspace-configuration
+$CustomLog = @"
+{
+    "customLogName": "sampleCustomLog1", 
+    "description": "Example custom log datasource", 
+    "inputs": [
+        { 
+            "location": { 
+            "fileSystemLocations": { 
+                "windowsFileTypeLogPaths": [ "e:\\iis5\\*.log" ],  
+                "linuxFileTypeLogPaths": [ "/var/logs" ] 
+                } 
+            }, 
+        "recordDelimiter": { 
+            "regexDelimiter": { 
+                "pattern": "\\n", 
+                "matchIndex": 0, 
+                "matchIndexSpecified": true, 
+                "numberedGroup": null 
+                } 
+            } 
+        }
+    ], 
+    "extractions": [
+        { 
+            "extractionName": "TimeGenerated", 
+            "extractionType": "DateTime", 
+            "extractionProperties": { 
+                "dateTimeExtraction": { 
+                    "regex": null, 
+                    "joinStringRegex": null 
+                    } 
+                } 
+            }
+        ] 
+    }
+"@
+# Custom Logs
+New-AzureRmOperationalInsightsCustomLogDataSource -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName -CustomLogRawJson "$CustomLog" -Name "Example Custom Log Collection"
